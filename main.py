2a994d8f3ba550f6a04cb694d655ec4649340bd7
juseok <juseok.lee@monitorapp.com>
2022-12-06 15:51:40 +0900

    edit compare_notion()

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class BP_CVE_Notion:
         self.databaseId = os.environ.get("DATABASE_ID")
         self.notion_data = None
         self.local_data = None
+        self.compare_df = None
         
     def get_notion_db(self):
         bp_cve = self.client.databases.query(
@@ -96,19 +97,36 @@ class BP_CVE_Notion:
         df['EndDate'] = df['properties'].apply(lambda x: x['날짜']['date']['end'])
         
         df = df.sort_values('Name').reset_index(drop=True)[['Name', 'id', 'Status','StartDate', 'EndDate']]
-        print(df)
         self.notion_data = df.copy()
         return
-        
+    
     def compare_notion(self):
-        if self.local_data == None:
-            pass
+        if self.local_data is not None:
+            if not self.local_data.equals(self.notion_data):
+                merge_df = pd.merge(self.notion_data, self.local_data, how='outer', on=['Name','id'])
+                compare_df = merge_df[merge_df['Status_x'] != merge_df['Status_y']]
+                compare_df = compare_df[['Name','id', 'Status_x', 'StartDate_x', 'EndDate_x']].rename(columns={'Status_x':'Status', 'StartDate_x':'StartDate', 'EndDate_x':'EndDate'})
+                self.compare_df = compare_df.copy()
+        self.local_data = self.notion_data.copy()
+            
+    
+    def get_local_data(self, path='./BP-CVE-Data/', filename='bp_cve_list.csv'):
+        if os.path.isfile(path+filename) :
+            self.local_data = pd.read_csv(path+filename, index_col = 0)
+    
+    def save_local_data_df(self, path='./BP-CVE-Data/', filename='bp_cve_list.csv'):
+        self.local_data.to_csv(path+filename)
         
 
 
 def main():
     gnd = BP_CVE_Notion()
     gnd.get_notion_db()
+    gnd.get_local_data()
+    gnd.compare_notion()
+    
+    
+    gnd.save_local_data_df()
     return
 
 
