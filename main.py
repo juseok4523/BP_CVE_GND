9d83b9d2aa5944518b978cd8a22d9ae5ee96e122
juseok <juseok.lee@monitorapp.com>
2022-12-07 11:39:23 +0900

    edit comparedata and add save notion data md

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import json
 import pandas as pd
 import numpy as np
 import os
+from notion2md.exporter.block import MarkdownExporter, StringExporter
+import multiprocessing as mp
 
 class BP_CVE_Notion:
     def __init__(self):
@@ -15,6 +17,8 @@ class BP_CVE_Notion:
         self.notion_data = None
         self.local_data = None
         self.compare_df = None
+        self.save_dir = './BP-CVE-Data/BP_CVE'
+        
         
     def get_notion_db(self):
         bp_cve = self.client.databases.query(
@@ -105,28 +109,38 @@ class BP_CVE_Notion:
             if not self.local_data.equals(self.notion_data):
                 merge_df = pd.merge(self.notion_data, self.local_data, how='outer', on=['Name','id'])
                 compare_df = merge_df[merge_df['Status_x'] != merge_df['Status_y']]
-                compare_df = compare_df[['Name','id', 'Status_x', 'StartDate_x', 'EndDate_x']].rename(columns={'Status_x':'Status', 'StartDate_x':'StartDate', 'EndDate_x':'EndDate'})
-                self.compare_df = compare_df.copy()
-        self.local_data = self.notion_data.copy()
+                compare_df = compare_df[['Name','id', 'Status_x', 'StartDate_x', 'EndDate_x']].rename(columns={'Status_x':'Status', 'StartDate_x':'StartDate', 'EndDate_x':'EndDate'}).dropna(axis=0)
+                if compare_df is not None:
+                    self.local_data = pd.concat([self.local_data, compare_df], axis=0, join='outer')
+                    self.compare_df = compare_df.copy()
+        else:
+            self.compare_df = self.notion_data.copy()
+            self.local_data = self.notion_data.copy()
+        return
             
-    
     def get_local_data(self, path='./BP-CVE-Data/', filename='bp_cve_list.csv'):
         if os.path.isfile(path+filename) :
             self.local_data = pd.read_csv(path+filename, index_col = 0)
+        return
     
     def save_local_data_df(self, path='./BP-CVE-Data/', filename='bp_cve_list.csv'):
         self.local_data.to_csv(path+filename)
+        return
         
-
+    def change_notion_md(self, n_cores=8):
+        temp_df = self.compare_df.copy()
+        temp_df.apply(lambda row: MarkdownExporter(block_id=row['id'], output_path=self.save_dir, output_filename=row['Name'], unzipped=True, download=True).export(), axis=1)
+        return
 
 def main():
     gnd = BP_CVE_Notion()
     gnd.get_notion_db()
     gnd.get_local_data()
     gnd.compare_notion()
+    if gnd.compare_df is not None:
+        gnd.change_notion_md()
     
-    
-    gnd.save_local_data_df()
+        gnd.save_local_data_df()
     return
 
 
